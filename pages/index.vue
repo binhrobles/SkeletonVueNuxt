9cf0052f4c488a6e5091d54a4828ac40b3ba5ae2
Binh 🌱 <binhrobles@gmail.com>
2020-08-24 10:54:08 -0700

    remove osx specific font family + build before ui test

--- pages/index.vue
+++ pages/index.vue
@@ -42,8 +42,7 @@ export default {}
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
